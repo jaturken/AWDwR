3c2d78dd46dc62cd80a973aedb41f9dc09a5038c
Alex Teut <jaturken@gmail.com>
2012-03-19 23:28:21 +0300

    Removed 'work' folder, its content is moved to the root of the repo.

--- work/demo/app/controllers/say_controller.rb~
+++
@@ -1,9 +0,0 @@
-class SayController < ApplicationController
-  def hello
-    @time = Time.now
-    @files = Dir.glob('*')
-  end
-
-  def goodbye
-  end
-end
--- work/demo/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Demo::Application.config.secret_token = '4670a03ddf37253c7e82b5c2f1e010b2c6755645ae5a5adba7b709f5e498cc809f34895b052d72925e1dab190ab9b5f062253b0f0a28714b165170f79ff41455'
