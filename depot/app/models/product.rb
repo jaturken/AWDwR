a4790fde3bb2dc1ca5a3a32b70d365ec42f6c7eb
Alex Teut <jaturken@gmail.com>
2012-03-26 18:07:53 +0300

    Iteration D2: Connecting Products to Carts

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
   validates :title, :description, :image_url, presence:true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness:true, length: {minimum: 10, message: "must be at least 10 characters long"}
@@ -6,4 +9,17 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)$}i, 
     message: 'Must be a URL for GIF, JPG or PNG image'
   }
+  
+  private
+  
+    #ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+	return true
+      else
+	error.add(:base, 'Line Items present')
+	return false
+      end
+    end
+
 end
