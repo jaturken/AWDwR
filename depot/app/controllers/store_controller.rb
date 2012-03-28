40fcdfe4815cf96f92250cc4ac3ac208714569e1
Alex Teut <jaturken@gmail.com>
2012-03-28 14:04:08 +0300

    Playtime after D: added counter of store views.

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,5 +1,19 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @counter = visit_counter
   end
+  
+  private 
+  
+  def visit_counter
+    if session[:counter].nil?
+      session[:counter]= 0
+    end
+    session[:counter] = session[:counter]+1
+  end
+     
+      
+    
+  
 end
