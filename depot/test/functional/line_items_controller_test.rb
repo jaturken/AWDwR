0cf49ab001af972ef160085978ff4e65dd7f807e
Alex Teut <jaturken@gmail.com>
2012-04-03 15:19:02 +0300

    Iteration F_last: updated functional tests.

--- depot/test/functional/line_items_controller_test.rb
+++ depot/test/functional/line_items_controller_test.rb
@@ -29,7 +29,7 @@ class LineItemsControllerTest < ActionController::TestCase
       post :create, product_id: products(:ruby).id
     end
 
-    assert_redirected_to cart_path(assigns(:line_item).cart)
+    assert_redirected_to store_url
   end
 
   test "should show line_item" do
@@ -52,6 +52,6 @@ class LineItemsControllerTest < ActionController::TestCase
       delete :destroy, id: @line_item.to_param
     end
 
-    assert_redirected_to line_items_path
+    assert_redirected_to store_url
   end
 end
