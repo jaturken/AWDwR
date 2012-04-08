3928c560c88833d4d6d8bbc52ddcd9281a121485
Alex Teut <jaturken@gmail.com>
2012-04-08 13:02:49 +0300

    Fixed error with non-founded images for products.

--- depot/db/seeds.rb
+++ depot/db/seeds.rb
@@ -27,7 +27,7 @@ Product.create(:title => 'Web Design for Developers',
         and work through a web site redesign, taking a new design from concept
         all the way to implementation.
       </p>},
-  :image_url =>   '/images/wd4d.jpg',    
+  :image_url =>   '/assets/wd4d.jpg',    
   :price => 42.95)
 # . . .
 Product.create(:title => 'Programming Ruby 1.9',
@@ -37,7 +37,7 @@ Product.create(:title => 'Programming Ruby 1.9',
         out there. If you need to get working programs delivered fast,
         you should add Ruby to your toolbox.
       </p>},
-  :image_url => '/images/ruby.jpg',
+  :image_url => '/assets/ruby.jpg',
   :price => 49.50)
 # . . .
 
@@ -51,5 +51,5 @@ Product.create(:title => 'Rails Test Prescriptions',
         procedures for Rails 2 and Rails 3, and introduces popular add-ons,
         including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
       </p>},
-  :image_url => '/images/rtp.jpg',
+  :image_url => '/assets/rtp.jpg',
   :price => 43.75)
