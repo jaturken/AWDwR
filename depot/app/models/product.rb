977baf687947f682de44721761030418c49967f7
Alex Teut <jaturken@gmail.com>
2012-04-08 14:03:02 +0300

    Fixed formatting for models

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -13,14 +13,14 @@ class Product < ActiveRecord::Base
   
   private
   
-    #ensure that there are no line items referencing this product
-    def ensure_not_referenced_by_any_line_item
-      if line_items.empty?
-	return true
-      else
-	error.add(:base, 'Line Items present')
-	return false
-      end
+  #ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      error.add(:base, 'Line Items present')
+      return false
     end
+  end
 
 end
