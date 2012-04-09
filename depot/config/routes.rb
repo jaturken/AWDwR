1984c39be7bd8a56823314e78c61e6c884fbf93d
Alex Teut <jaturken@gmail.com>
2012-04-09 18:04:41 +0300

    Iteration G2: added Atom feed.

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -13,7 +13,9 @@ resources :line_items do
 
   get "store/index"
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
  
   # The priority is based upon order of creation:
   # first created -> highest priority.
