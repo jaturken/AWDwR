39dfa912b01df577d8fac723a1045096077843e8
Alex Teut <jaturken@gmail.com>
2012-04-11 11:58:06 +0300

    Playtime after G: added XML, HTML and JSON views for who_bought product method.

--- depot/app/controllers/products_controller.rb
+++ depot/app/controllers/products_controller.rb
@@ -85,6 +85,9 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     respond_to do |format|
       format.atom
+      format.xml { render :xml => @product.to_xml(:include => :orders) }
+      format.json {render :json => @product.to_json(:include => :orders)}
+      format.html
     end
   end
 
