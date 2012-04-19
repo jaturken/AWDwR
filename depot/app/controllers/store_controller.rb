d7926cfa3f3d6e8b2fb22f2e67fc7278d28df630
Alex Teut <jaturken@gmail.com>
2012-04-19 13:42:59 +0300

    Iteration J4: added locale switcher

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,8 +1,12 @@
 class StoreController < ApplicationController
   def index
-    @products = Product.order(:title)
-    @counter = visit_counter
-    @cart = current_cart
+    if params[:set_locale]
+      redirect_to store_path(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+      @counter = visit_counter
+      @cart = current_cart
+    end
   end
   
   private 
