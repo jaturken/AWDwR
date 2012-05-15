5453a50c0ef7eae72121f8ca5a09f7eea055372d
Alex Teut <jaturken@gmail.com>
2012-05-15 11:57:32 +0300

    Updated routes

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,5 +1,10 @@
 Depot::Application.routes.draw do
+
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
  
+  resources :comments
+
   get "store/index"
 
   scope'(:locale)' do
@@ -16,12 +21,7 @@ Depot::Application.routes.draw do
     end
     root to: 'store#index', as: 'store'
   end
-
-
  
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -65,10 +65,7 @@ Depot::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
     
-  # See how all your routes lay out with "rake routes"
-
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
